bbcd292e605f04405a730ac03c72621e4756687b
asd5361 <corey5361@nate.com>
2020-10-15 12:50:37 +0900

    1015_sBbooks

---
+++ 06xmljson/src/main/java/com/example/a06xmljson/SBooks.java
@@ -0,0 +1,13 @@
+package com.example.a06xmljson;
+
+class SBooks {
+    String author;
+    String title;
+    Float price;
+
+    public SBooks(String author,String title, Float price) {
+        this.author = author;
+        this.title = title;
+        this.price = price;
+    }
+}
